7c140ba9bb9fb2705a0d8f843eb60b116c0affad
Alaf Azam <alafazam@gmail.com>
2016-01-02 02:53:57 +0530

    add io.on events

--- easy_chat/app.js
+++ easy_chat/app.js
@@ -33,6 +33,8 @@ app.get('/users', user.list);
 
 var server = http.createServer(app)
 var io = require("socket.io").listen(server);
+var connections = [];
+var numberOfUsers = 0;
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
@@ -41,5 +43,41 @@ server.listen(app.get('port'), function(){
 
 
 io.on('connection', function(socket){
-	console.log("working");
+    numberOfUsers++;
+
+
+    socket.username = '';
+
+    socket.emit('request login',{'id':socket.id});
+
+    socket.on('username',function (data) {
+        socket.username = data.username;
+        connections[numberOfUsers-1] = {
+            username:socket.username,
+            id:socket.id
+        };
+        socket.broadcast.emit('update users', {
+          status:'joined',
+          numberOfUsers: numberOfUsers,
+          connections:connections,
+          msg:socket.username+' joined'
+        });
+    })
+
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message',{'username':socket.username,'msg':msg});
+        console.log(socket.username + ' said ' + msg);
+    });
+
+
+    socket.on('disconnect', function(){
+        console.log(socket.username +' got disconnected');
+        numberOfUsers--;
+        socket.broadcast.emit('update users', {
+          status:'disconnect',
+          numberOfUsers: numberOfUsers,
+          msg:socket.username+' got disconnected'
+        });
+    });
 });
