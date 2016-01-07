f3907f9a34166adfa02ee1a9b66694cf4da59e0f
Alaf Azam <alafazam@gmail.com>
2016-01-08 01:24:57 +0530

    validation for usernam

--- connection.js
+++ connection.js
@@ -1,49 +1,73 @@
+var _ = require('lodash');
+
 var connectionList = [];
 var numberOfUsers = 0;
 
 exports.connectionList = connectionList;
 exports.numberOfUsers = numberOfUsers;
 
-exports.userConnection = function (socket) {
-	numberOfUsers++;
+exports.userConnection = function(socket) {
+    numberOfUsers++;
     socket.username = '';
 
-	//  make user tell his name
-    socket.emit('request login',{'id':socket.id});
+    //  make user tell his name
+    socket.emit('request login', {
+        'id': socket.id
+    });
 
-	// when we know who use is
-    socket.on('username',function (data) {
-        socket.username = data.username;
-        connectionList[numberOfUsers-1] = {
-            username:socket.username,
-            id:socket.id
-        };
-        // notify others
-        socket.broadcast.emit('joined', {
-          username:socket.username
+    // when we know who use is
+    socket.on('username', function(data) {
+        var username = data.username;
+        var nameExists = _.some(connectionList, function(item) {
+            return item.username == username;
         });
+        if (nameExists) {
+            socket.emit('usernameExist', {
+                'username': username
+            });
+        } else {
+            socket.username = data.username;
+            connectionList[numberOfUsers - 1] = {
+                username: socket.username,
+                id: socket.id
+            };
+            // notify others
+            socket.broadcast.emit('joined', {
+                username: socket.username
+            });
+        }
     })
 
 
-    socket.on('message', function(msg){
-        socket.broadcast.emit('message',{'username':socket.username,'msg':msg});
-        socket.emit('message',{'username':socket.username,'msg':msg});
+    socket.on('message', function(msg) {
+        socket.broadcast.emit('message', {
+            'username': socket.username,
+            'msg': msg
+        });
+        socket.emit('message', {
+            'username': socket.username,
+            'msg': msg
+        });
         console.log(socket.username + ' said ' + msg);
     });
 
-      socket.on('start typing', function(msg){
-        socket.broadcast.emit('typing',{'username':msg.username,'id':msg.id});
+    socket.on('start typing', function(msg) {
+        socket.broadcast.emit('typing', {
+            'username': msg.username,
+            'id': msg.id
+        });
         console.log(msg.username + ' is typing');
     });
 
 
 
 
-    socket.on('disconnect', function(){
-        console.log(socket.username +' got disconnected');
+
+    socket.on('disconnect', function() {
+        console.log(socket.username + ' got disconnected');
         numberOfUsers--;
         socket.broadcast.emit('left', {
-          username:socket.username
+            username: socket.username
         });
     });
 
--- public/javascripts/connection.js
+++ public/javascripts/connection.js
@@ -1,80 +1,119 @@
+(function() {
 
-(function(){
+    // crazy way of eascaping
+    function escapeHtml(str) {
+        var div = document.createElement('div');
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
+    };
 
-// crazy way of eascaping
-  function escapeHtml(str) {
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-
-  var msg_box = document.getElementById('m');
-
-
-  var username = '';
-  var socket = io();
-  var message_window = document.getElementById('messages-window'); 
-  $('form').submit(function(){
-    if(!$('#m').val()||$('#m').val() == '')
-      return false;
-
-    socket.emit('message', $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-
-  socket.on('message', function(data){
-    var li = document.createElement("li");
-
-    classie.add(li,"message_li animated bounceInLeft tada");
-
-    var message_u = document.createElement("span");
-    message_u.appendChild(document.createTextNode(data.username));
-    classie.add(message_u,"messages message_u");
-
-    var message_ts = document.createElement("span");
-    message_ts.appendChild(document.createTextNode('12:30 PM'));
-    classie.add(message_ts,"messages message_ts");
-
-    var message_t = document.createElement("span");
-    message_t.appendChild(document.createTextNode(data.msg));
-    classie.add(message_t,"messages message_t");
-
-    li.appendChild(message_u);
-    li.appendChild(message_ts);
-    li.appendChild(message_t);
-    if(data.username == username){
-      classie.add(li,"right");
-      classie.add(message_t,"right");
+    function makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
+        for (var i = 0; i < 10; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
     }
-    message_window.appendChild(li);
-  });
 
-  socket.on('joined', function (data) {
-    $.notify(data.username + " has joined.", "info");
-  });
 
-  socket.on('left', function (data) {
-    $.notify(data.username + " has left.", "info");
-  });
 
-  socket.on('request login', function(data){
-    id = data.id;
-    username = prompt('please enter your usename') || 'newUser';
-    socket.emit('username',{'username':username,'id':id});
-  });
+    function askUsername(again) {
+        if (!again)
+            return prompt('please enter your usename or cancel to use a random') || makeid();
+        else if (again == 'exist')
+            return prompt('username exist please enter another or cancel to use a random') || makeid();
+        else if (again == 'invalid')
+            return prompt('username is invalid please enter another or cancel to use a random') || makeid();
+    }
 
-  // msg_box.onkeypress = function (e) {
-  //   if((String.fromCharCode(e.which) || msg_box.value) && msg_box.value !== ' ' ){
-  //     console.log('true');
-  //     socket.emit('start typing',{'username':username,'id':id});
-  //   }else{
-  //       console.log('fasle');
-  //     socket.emit('stop typing',{'username':username,'id':id});      
-  //   }
-  // }
+    function validateUsername(username) {
+        if (username && username.length > 2 && username.length <= 15 && username != '' && /^[a-zA-Z_]+$/.test(username))
+            return username;
+        else false;
+    }
+
+    var msg_box = document.getElementById('m');
+
+
+    var username = '';
+    var socket = io();
+    var id = null;
+
+    var message_window = document.getElementById('messages-window');
+    $('form').submit(function() {
+        if (!$('#m').val() || $('#m').val() == '')
+            return false;
+
+        socket.emit('message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+    socket.on('message', function(data) {
+        var li = document.createElement("li");
+
+        classie.add(li, "message_li animated bounceInLeft tada");
+
+        var message_u = document.createElement("span");
+        message_u.appendChild(document.createTextNode(data.username));
+        classie.add(message_u, "messages message_u");
+
+        var message_ts = document.createElement("span");
+        message_ts.appendChild(document.createTextNode('12:30 PM'));
+        classie.add(message_ts, "messages message_ts");
+
+        var message_t = document.createElement("span");
+        message_t.appendChild(document.createTextNode(data.msg));
+        classie.add(message_t, "messages message_t");
+
+        li.appendChild(message_u);
+        li.appendChild(message_ts);
+        li.appendChild(message_t);
+        if (data.username == username) {
+            classie.add(li, "right");
+            classie.add(message_t, "right");
+        }
+        message_window.appendChild(li);
+    });
+
+    socket.on('joined', function(data) {
+        $.notify(data.username + " has joined.", "info");
+    });
+
+    socket.on('left', function(data) {
+        $.notify(data.username + " has left.", "info");
+    });
+
+    socket.on('request login', function(data) {
+        username = askUsername();
+        while (!validateUsername(username)) {
+            username = askUsername('invalid');
+        }
+        socket.emit('username', {
+            'username': username,
+        });
+    });
+
+    socket.on('usernameExist', function(data) {
+      username = askUsername('exist');
+        while (!validateUsername(username)) {
+            username = askUsername('invalid');
+        }
+        socket.emit('username', {
+            'username': username,
+        });
+    });
+
+
+    // msg_box.onkeypress = function (e) {
+    //   if((String.fromCharCode(e.which) || msg_box.value) && msg_box.value !== ' ' ){
+    //     console.log('true');
+    //     socket.emit('start typing',{'username':username,'id':id});
+    //   }else{
+    //       console.log('fasle');
+    //     socket.emit('stop typing',{'username':username,'id':id});      
+    //   }
+    // }
 
 
 })();
-
