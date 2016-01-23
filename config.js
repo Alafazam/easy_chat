410668bd543378c1da71f3256728194861907411
Alaf Azam <alafazam@gmail.com>
2016-01-23 19:33:03 +0530

    add config

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {};
+
+config.session = {
+	secret: process.env.SESSION_SECRET || 'username', 
+	resave: true,
+	saveUninitialized: true 
+}
+
+
+
+config.server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+config.server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+module.exports = config;
