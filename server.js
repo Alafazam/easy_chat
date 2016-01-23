4df9fd82a652067994ac2b5f55bde76880dc0571
Alaf Azam <alafazam@gmail.com>
2016-01-23 14:24:06 +0530

    fix server.js

--- server.js
+++ server.js
@@ -1,61 +1,103 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+
+
+var routes = require('./routes/index');
 var user = require('./routes/user');
 var chatroom = require('./routes/chatroom');
+// var profile = require('routes/profile');
 
-var http = require('http');
-var path = require('path');
 var connection = require('./connection');
 // var rooms = require('./rooms');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var app = express();
 
 // all environments
 app.set('port', server_port || process.env.PORT || 3000);
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
-app.get('/', routes.index);
-app.get('/chatroom', chatroom.chatroom);
+
+// app.locals.rooms = ["one","two"];
+
+app.set("rooms",["one","two"]);
+
+app.get('/', routes);
+app.use('/chatroom', chatroom);
+
+
+// app.get('/chatroom/list', chatroom.chatroom);
+
+// app.get('/chatroom/:roomName', chatroom.chatroom);
+
+
 
 // app.get('/users', user.list);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+
 
 var server = http.createServer(app)
 var io = require("socket.io").listen(server);
 var connections = [];
 var numberOfUsers = 0;
 
-// server.listen(app.get('port'), function(){
-//   console.log('Express server listening on port ' + app.get('port'));
-// });
+
 
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
 });
 
 io.on('connection',connection.userConnection);
-
-
-// rooms.P();
-
-// var t = new rooms();
