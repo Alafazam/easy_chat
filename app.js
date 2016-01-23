e757fb7799c1cc881694cdd83012819e6dd9d4b9
Alaf Azam <alafazam@gmail.com>
2016-01-23 14:16:17 +0530

    back to port 8080

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var chatroom = require('./routes/chatroom');
 var connection = require('./connection');
 // var rooms = require('./rooms');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var app = express();
