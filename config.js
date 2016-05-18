c90f605d6addaec00437a340859a1f247764dd3a
Alaf Azam <alafazam@gmail.com>
2016-05-18 22:56:20 +0530

    Touch cookies one every request
    Now timeout for cookie expiry is reset at every request

--- config.js
+++ config.js
@@ -3,10 +3,12 @@ var config = {};
 config.session = {
 	secret: process.env.SESSION_SECRET || 'username',
 	resave: true,
-	saveUninitialized: true
+	saveUninitialized: true,
+	cookie: {
+    	rolling: true,
+    	maxAge: 60*60*1000
+  	}
 }
-
-
 
 config.server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 config.server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
